e0f6965ef6a7b4b03bdec3cccb60686489237a3d
Mats Wilhelm <matschew@web.de>
2014-03-27 21:57:02 +0100

    a

---
+++ Server/Server/Factories/FactoryEnums/CreatureEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server.Factories.FactoryEnums
+{
+    enum CreatureEnum
+    {
+        HUMAN_MALE,
+        HUMAN_FEMALE,
+        ORC_MALE,
+        ORC_FEMALE
+    }
+}
