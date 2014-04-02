7636ef0cd89dc657cc27f731ef1962e1e545a02c
Gothen111 <gothen92@yahoo.de>
2014-04-02 19:23:15 +0200

    Fonts :)

---
+++ Server/Server/Ressourcen/RessourcenManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using Server.Ressourcen.Font;
+
+namespace Server.Ressourcen
+{
+    class RessourcenManager
+    {
+        public static RessourcenManager ressourcenManager = new RessourcenManager();
+        public static Dictionary<String, SpriteFont> fonts = new Dictionary<String, SpriteFont>();
+
+        public RessourcenManager()
+        {
+        }
+
+        public void loadGeneral(ContentManager _ContentManager)
+        {
+            addFont("Arial", _ContentManager.Load<SpriteFont>("Font/Arial"));
+        }
+
+        public void addFont(String _Name, SpriteFont _SpiteFont)
+        {
+            if(!this.containFont(_Name, _SpiteFont))
+            {
+                fonts.Add(_Name, _SpiteFont);
+            }
+        }
+
+        public bool containFont(String _Name, SpriteFont _SpiteFont)
+        {
+            return fonts.ContainsKey(_Name) || fonts.ContainsValue(_SpiteFont);
+        }
+    }
+}
