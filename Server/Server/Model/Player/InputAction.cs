2df57fcddd6f4b4cb2785733c8a374bc44a3fb14
Gothen111 <gothen92@yahoo.de>
2014-05-02 22:13:57 +0200

    Player Idee

---
+++ Server/Server/Model/Player/InputAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server.Model.Player
+{
+    class InputAction
+    {
+        public void performAction()
+        {
+        }
+    }
+}
---
+++ Server/Server/Model/Player/PlayerContoller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Server.Model.Object;
+namespace Server.Model.Player
+{
+    class PlayerContoller
+    {
+        private PlayerObject playerObject;
+        private List<InputAction> inputActions;
+
+        public PlayerContoller(PlayerObject _PlayerObject)
+        {
+            this.playerObject = _PlayerObject;
+            this.inputActions = new List<InputAction>();
+        }
+
+        public void update()
+        {
+            foreach (InputAction var_InputAction in this.inputActions)
+            {
+                var_InputAction.performAction();
+            }
+        }
+
+        private void perfomFirstAction()
+        {
+            if (this.inputActions.Count > 0)
+            {
+                this.inputActions.ElementAt(0).performAction();
+                this.inputActions.RemoveAt(0);
+            }
+        }
+
+        public void addInputAction(InputAction _InputAction)
+        {
+            this.inputActions.Add(_InputAction);
+        }
+    }
+}
