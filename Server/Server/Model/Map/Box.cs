2ba9f86ffe15d2231a7a2150c0c930f8cb016d36
Gothen111 <gothen92@yahoo.de>
2014-05-10 19:25:26 +0200

    Box

---
+++ Server/Server/Model/Map/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+namespace Server.Model.Map
+{
+    class Box
+    {
+        private int id;
+        private Vector2 size;
+
+        private Box topNeighbour;
+        private Box leftNeighbour;
+        private Box rightNeighbour;
+        private Box bottomNeighbour;
+
+        private Vector2 position;
+
+        private String name;
+
+        private Box parent;
+
+        public virtual void update()
+        {
+
+        }
+    }
+}
