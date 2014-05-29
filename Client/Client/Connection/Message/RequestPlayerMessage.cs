a09bcb75efbe4844605141dd9cb623e61502e5f7
Gothen111 <gothen92@yahoo.de>
2014-05-29 11:27:02 +0200

    hut hut

---
+++ Client/Client/Connection/Message/RequestPlayerMessage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lidgren.Network;
+using Lidgren.Network.Xna;
+
+namespace Client.Connection.Message
+{
+    class RequestPlayerMessage : IGameMessage
+    {
+        #region Constructors and Destructors
+
+        public RequestPlayerMessage(NetIncomingMessage im)
+        {
+            this.Decode(im);
+        }
+
+        public RequestPlayerMessage(String _PlayerName)
+        {
+            this.Name = _PlayerName;
+            this.MessageTime = NetTime.Now;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public String Name { get; set; }
+
+        public double MessageTime { get; set; }
+
+
+        #endregion
+
+        #region Public Methods
+
+        public EIGameMessageType MessageType
+        {
+            get { return EIGameMessageType.UpdateChunkMessage; }
+        }
+
+        public void Decode(NetIncomingMessage im)
+        {
+            this.Name = im.ReadString();
+            this.MessageTime = im.ReadDouble();
+        }
+
+        public void Encode(NetOutgoingMessage om)
+        {
+            om.Write(this.Name);
+            om.Write(this.MessageTime);
+        }
+
+        #endregion
+    }
+}
---
+++ Client/Client/Connection/Message/UpdatePlayerMessage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lidgren.Network;
+using Lidgren.Network.Xna;
+
+namespace Client.Connection.Message
+{
+    class UpdatePlayerMessage : IGameMessage
+    {
+        #region Constructors and Destructors
+
+        public UpdatePlayerMessage(NetIncomingMessage im)
+        {
+            this.Decode(im);
+        }
+
+        public UpdatePlayerMessage(Model.Object.PlayerObject _PlayerObject)
+        {
+            this.Id = _PlayerObject.Id;
+            this.MessageTime = NetTime.Now;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        public double MessageTime { get; set; }
+
+
+        #endregion
+
+        #region Public Methods
+
+        public EIGameMessageType MessageType
+        {
+            get { return EIGameMessageType.UpdateChunkMessage; }
+        }
+
+        public void Decode(NetIncomingMessage im)
+        {
+            this.Id = im.ReadInt32();
+            this.MessageTime = im.ReadDouble();
+        }
+
+        public void Encode(NetOutgoingMessage om)
+        {
+            om.Write(this.Id);
+            om.Write(this.MessageTime);
+        }
+
+        #endregion
+    }
+}
