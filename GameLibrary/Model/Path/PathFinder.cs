888016057bd0602f3424a5e44e7056e3d8dc0d7c
Gothen111 <gothen92@yahoo.de>
2014-07-11 18:21:42 +0200

    pathfinding ....

--- GameLibrary/Model/Path/PathFinder.cs
+++ GameLibrary/Model/Path/PathFinder.cs
@@ -58,14 +58,14 @@ namespace GameLibrary.Model.Path
 
         private static List<Vector2> shortestWayRekursive(List<Block> _ListVisited, Block _CurrentBlock, Block _EndBlock, List<Vector2> _Way)
         {
+            _ListVisited.Add(_CurrentBlock);
+            _Way.Add(_CurrentBlock.Position);
+
             if(_CurrentBlock.Equals(_EndBlock))
             {
                 return _Way;
             }
-
-            _ListVisited.Add(_CurrentBlock);
-            _Way.Add(_CurrentBlock.Position); // TODO: KOPIE von _Way.... Sonst gibts n problem :/
-
+            
             Block var_BlockRight = (Block)_CurrentBlock.RightNeighbour;
             Block var_BlockLeft = (Block)_CurrentBlock.LeftNeighbour;
             Block var_BlockTop = (Block)_CurrentBlock.TopNeighbour;
@@ -77,17 +77,51 @@ namespace GameLibrary.Model.Path
                 {
                     if (var_BlockRight.IsWalkAble)
                     {
-                        shortestWayRekursive(_ListVisited, var_BlockRight, _EndBlock, new List<Vector2>(_Way)); // TODO: KOPIE von _Way.... Sonst gibts n problem :/
+                        shortestWayRekursive(_ListVisited, var_BlockRight, _EndBlock, new List<Vector2>(_Way));
                     }
                 }
             }
+
+            else if (_EndBlock.Position.X < _CurrentBlock.Position.X)
+            {
+                if (!_ListVisited.Contains(var_BlockLeft))
+                {
+                    if (var_BlockLeft.IsWalkAble)
+                    {
+                        shortestWayRekursive(_ListVisited, var_BlockLeft, _EndBlock, new List<Vector2>(_Way));
+                    }
+                }
+            }
+
+            if (_EndBlock.Position.Y > _CurrentBlock.Position.Y)
+            {
+                if (!_ListVisited.Contains(var_BlockBottom))
+                {
+                    if (var_BlockBottom.IsWalkAble)
+                    {
+                        shortestWayRekursive(_ListVisited, var_BlockBottom, _EndBlock, new List<Vector2>(_Way));
+                    }
+                }
+            }
+
+            if (_EndBlock.Position.Y < _CurrentBlock.Position.Y)
+            {
+                if (!_ListVisited.Contains(var_BlockTop))
+                {
+                    if (var_BlockTop.IsWalkAble)
+                    {
+                        shortestWayRekursive(_ListVisited, var_BlockTop, _EndBlock, new List<Vector2>(_Way));
+                    }
+                }
+            }
+
 
             /*
              * 
              * 
              */
 
-            _ListVisited.Add(new Block(0,0,0,null));
+            // TODO: PATHFINDER fertig machen! Rekusions abbruch usw .. 
             return new List<Vector2>();
         }
     }
