38aef98879383a3dd84ce535e9f6db6bec72c89c
Gothen111 <gothen92@yahoo.de>
2014-07-21 21:49:02 +0200

    dfg

---
+++ GameLibrary/Gui/ContainerManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace GameLibrary.Gui
+{
+    class ContainerManager
+    {
+        public static ContainerManager containerManager = new ContainerManager();
+
+        private Container activeContainer;
+
+        public Container ActiveContainer
+        {
+            get { return activeContainer; }
+            set { activeContainer = value; }
+        }
+
+        Container mainMenu;
+
+        private ContainerManager()
+        {
+            this.createMainMenu();
+        }
+
+        public void createMainMenu()
+        {
+            this.mainMenu = new Container(new Rectangle(0,0,500,500));
+            Button var_Button = new Button(new Rectangle(100,100,50,50));
+            var_Button.Action = 
+            this.mainMenu.add(var_Button);
+        }
+    }
+}
