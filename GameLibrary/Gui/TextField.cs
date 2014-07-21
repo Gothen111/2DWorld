d304b146a1200ec6bf4330d2d2d63b7a99efb448
Crashkurs <matschew@web.de>
2014-07-21 23:03:05 +0200

    hu2

--- GameLibrary/Gui/TextField.cs
+++ GameLibrary/Gui/TextField.cs
@@ -47,7 +47,8 @@ namespace GameLibrary.Gui
             {
                 if (buttonPressed == Microsoft.Xna.Framework.Input.Keys.Back)
                 {
-                    this.text = this.text.Substring(0, this.text.Length - 1);
+                    if(this.text.Length > 0)
+                        this.text = this.text.Substring(0, this.text.Length - 1);
                 }
                 else if (buttonPressed == Microsoft.Xna.Framework.Input.Keys.LeftShift || buttonPressed == Microsoft.Xna.Framework.Input.Keys.LeftAlt)
                 {
@@ -55,14 +56,7 @@ namespace GameLibrary.Gui
                 }
                 else
                 {
-                    if (Peripherals.KeyboardManager.shiftPressed)
-                    {
-                        this.text += buttonPressed.ToString().ToUpper();
-                    }
-                    else
-                    {
-                        this.text += buttonPressed.ToString().ToLower();
-                    }
+                    this.text += Peripherals.KeyboardManager.TryConvertKey(buttonPressed);
                 }
             }
         }
